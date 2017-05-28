65cd58db36c1fbc5be82896124b54f8721699595
Lorenzo Bernaschina <lorenzo@MacBook-Pro-di-Lorenzo.local>
2017-05-28 11:00:08 +0200

    Directory tree aggiornato

--- src/main/java/it/polimi/ingsw/rmiClient/RmiClient.java
+++ src/main/java/it/polimi/ingsw/rmiClient/RmiClient.java
@@ -1,8 +1,4 @@
-package it.polimi.ingsw.rmiClient;
-
-import it.polimi.ingsw.exceptions.LoginException;
-import it.polimi.ingsw.rmiServer.RmiServerInterface;
-import it.polimi.ingsw.server.AbstractPlayer;
+package it.polimi.ingsw;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -10,69 +6,31 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class RmiClient extends AbstractPlayer implements RmiClientInterface {
+public class RmiClient implements RmiClientInterface {
 
-    /**
-     * Registry for published object.
-     */
     private Registry registry;
-
-    /**
-     * Server RMI interface.
-     */
     private RmiServerInterface server;
 
-    /**
-     * SocketPlayer unique identifier on the server.
-     */
-    private String myID;
-
-    /**
-     * Class constructor.
-     */
     public RmiClient(){
         super();
     }
 
-    /**
-     * Method to connect client to server.
-     */
-    public void connect(){
+    public void connectToServer(){
         try {
             registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
             server = (RmiServerInterface) registry.lookup("RmiServerInterface");
             UnicastRemoteObject.exportObject(this, 0);
             registry.rebind("RmiClientInterface", this);
             //server.clientConnected();
-        }catch(RemoteException | NotBoundException e){
-            // throw Client Exception
+        }catch(RemoteException e){
+            System.out.println("Cannot connect the server - RemoteException");
+        }catch(NotBoundException e) {
+            System.out.println("Cannot connect the server - NotBoundException");
         }
     }
 
-    /**
-     * Login player using RMI Server interface method login().
-     * @param username
-     * @param password
-     */
-    public void login(String username, String password){
-        try {
-            myID = server.login(username, password, this);
-        }catch(LoginException e){
-            // da gestire
-        }
-    }
-
-    /**
-     * Signin player using RMI Server interface method signin().
-     * @param username
-     * @param password
-     */
-    public void signin(String username, String password){
-        try {
-            server.signin(username, password);
-        } catch(LoginException e){
-            // da gestire
-        }
+    public void serverConnected() throws RemoteException{
+        System.out.println("Sei connesso al server.");
     }
 
 }
